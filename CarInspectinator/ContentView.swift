9924703fc79008ddf4a23187eda4c1624e78cff1
oholmess <oholmes.ieu2023@student.ie.edu>
2025-09-23 14:50:29 +0200

    Initial Commit

---
+++ CarInspectinator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CarInspectinator
+//
+//  Created by Oliver Holmes on 9/23/25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
